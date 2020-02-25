164f137c78f9e84011e9c614dadd5c84793c0f33
habibzeeh <45460979+habibzeeh@users.noreply.github.com>
2020-02-25 16:39:12 +0000

    02/25/2020

--- src/com/property/manager/Views/SplashScreen.java
+++ src/com/property/manager/Views/SplashScreen.java
@@ -2,20 +2,26 @@ package com.property.manager.Views;
 
 import com.property.manager.Manager.AdminManager;
 import com.property.manager.Models.Admin;
+import com.property.manager.Views.utils.ImagePanel;
 import com.property.manager.test;
 
 import javax.swing.*;
 import java.util.List;
 
 public class SplashScreen {
-    private JButton loginButton;
-    private JTextArea propertyManagerSoftwareTextArea;
     private JPanel panel1;
 
     public static void main(String[] args){
+
+        ImagePanel backgroundImage = new ImagePanel(new ImageIcon("res\\drawable\\splash_background.jpg").getImage());
+
+        String p =  System.getProperty("user.dir");
+
+
         JFrame frame = new JFrame("Splash");
-        frame.setContentPane(new SplashScreen().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(backgroundImage);
+        //frame.setContentPane(new SplashScreen().panel1);
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
